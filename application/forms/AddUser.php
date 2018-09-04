10ea7af476fc1e1e5f995e13606cf5de9831af79
Reinhard <cr14@medma.ad.uni-heidelberg.de>
2018-09-04 13:13:35 +0200

    optimized form for adding users

--- application/forms/AddUser.php
+++ application/forms/AddUser.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * 
+ * @author Cedric Reinhard
+ *
+ */
 class Application_Form_AddUser extends Zend_Form
 {
-
     public function init()
     {
         // set method to post
@@ -11,122 +15,106 @@ class Application_Form_AddUser extends Zend_Form
         // add username field
         $this->addElement('text', 'username', array(
             'placeholder' => 'Benutzername',
-            'required' => true,
-            'filters' => array(
+            'required'    => true,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add givenname field
         $this->addElement('text', 'givenname', array(
             'placeholder' => 'Vorname',
-            'required' => true,
-            'filters' => array(
+            'required'    => true,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add surname field
         $this->addElement('text', 'surname', array(
             'placeholder' => 'Nachname',
-            'required' => true,
-            'filters' => array(
+            'required'    => true,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add gender select
         $this->addElement('select', 'gender', array(
-            'placeholder' => 'Geschlecht',
-            'required' => false,
-            'multiOptions' => array(
-                'm' => 'maennlich',
-                'f' => 'weiblich',
-                '-' => 'andere'
-            ),
-            'class' => 'form-control'
+            'placeholder'  => 'Geschlecht',
+            'required'     => false,
+            'multiOptions' => Utils_User_Gender::getDropdownOptionsArray(),
+            'class'        => 'form-control'
         ));
         
         // add password field
         $this->addElement('password', 'password', array(
             'placeholder' => 'Passwort',
-            'required' => true,
-            'filters' => array(
+            'required'    => true,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add email field
         $this->addElement('text', 'email', array(
             'placeholder' => 'E-Mail',
-            'required' => true,
-            'filters' => array(
+            'required'    => true,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'validators' => array(
+            'validators'  => array(
                 'EmailAddress',
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add email2 field
         $this->addElement('text', 'email2', array(
             'placeholder' => 'E-Mail (optional)',
-            'required' => false,
-            'filters' => array(
+            'required'    => false,
+            'filters'     => array(
                 'StringTrim'
             ),
-            'validators' => array(
+            'validators'  => array(
                 'EmailAddress',
             ),
-            'class' => 'form-control'
+            'class'       => 'form-control'
         ));
         
         // add organiztation select
         $this->addElement('select', 'organization', array(
-            'label' => 'Organisation',
-            'required' => false,
-            'multiOptions' => array(
-                '' => 'KEINE',
-                'FST' => 'FST',
-                'F&CO' => 'F&CO'
-            ),
-            'class' => 'form-control'
+            'label'        => 'Organisation',
+            'required'     => false,
+            'multiOptions' => Utils_User_Organization::getDropdownOptions(),
+            'class'        => 'form-control'
         ));
         
         // add role select
         $this->addElement('select', 'role', array(
-            'label' => 'Rolle',
-            'required' => true,
-            'multiOptions' => array(
-                'player' => 'Spieler',
-                'coach' => 'Coach'
-            ),
-            'class' => 'form-control'
+            'label'        => 'Rolle',
+            'required'     => true,
+            'multiOptions' => Utils_User_Role::getDropdownOptionsArray(),
+            'class'        => 'form-control'
         ));
         
         // add state select
         $this->addElement('select', 'state', array(
-            'label' => 'Status',
-            'required' => true,
-            'multiOptions' => array(
-                'active' => 'Aktiv',
-                'inactive' => 'Inaktiv',
-                'disabled' => 'Gesperrt'
-            ),
-            'class' => 'form-control'
+            'label'        => 'Status',
+            'required'     => true,
+            'multiOptions' => Utils_User_State::getDropdownOptionsArray(),
+            'class'        => 'form-control'
         ));
         
         // Add the submit button
         $this->addElement('submit', 'submit', array(
             'ignore' => true,
-            'class' => 'btn btn-lg btn-success btn-block',
-            'label' => 'Benutzer anlegen'
+            'class'  => 'btn btn-lg btn-success btn-block',
+            'label'  => 'Benutzer anlegen'
         ));
     }
 }
-
