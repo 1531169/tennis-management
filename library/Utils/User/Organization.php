e57157eeb460da67b479b044ae1154f31c400288
Reinhard <cr14@medma.ad.uni-heidelberg.de>
2018-09-05 07:54:34 +0200

    added function to create a HTML5 data list

--- library/Utils/User/Organization.php
+++ library/Utils/User/Organization.php
@@ -7,21 +7,49 @@
  */
 class Utils_User_Organization
 {
+    const DEFAULT_VAL = 'FREI';
+    const DEFAULT_KEY = '';
+    
     public static function getDropdownOptions()
     {
-        $mapper = new Application_Model_UserMapper();
-        $sql = $mapper->getDbTable()
-            ->select()
-            ->distinct()
-            ->from('user', 'organization');
-        $result = $mapper->getDbTable()->fetchAll($sql);
         $options = array(
-            '' => 'FREI'
+            self::DEFAULT_KEY => self::DEFAULT_VAL
         );
+        $result = self::getOrganizationArray();
         foreach ($result as $row) {
             $options[$row->organization] = $row->organization;
         }
         return $options;
+    }
+    
+    public static function getDatalistValues()
+    {
+        $options = array(
+            self::DEFAULT_VAL
+        );
+        $result = self::getOrganizationArray();
+        foreach ($result as $row) {
+            $options[] = $row->organization;
+        }
+        return $options;
+    }
+    
+    public static function getHtmlDatalist() {
+        $html = '<datalist id="organizations">';
+        foreach(self::getDatalistValues() as $option) {
+            $html .= '<option value="' . $option . '">';
+        }
+        $html .= '</datalist>';
+        return $html;
+    }
+    
+    private static function getOrganizationArray() {
+        $mapper = new Application_Model_UserMapper();
+        $sql    = $mapper->getDbTable()
+        ->select()
+        ->distinct()
+        ->from('user', 'organization');
+        return $mapper->getDbTable()->fetchAll($sql);
     }
 }
 
